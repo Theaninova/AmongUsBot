e8dfddc5d9f380c7049950dbd0bcd9928b86b4a1
Wieland Schöbl <wulkanat@gmail.com>
2020-10-20 23:45:24 +0200

    Accept ([A-Z]|\d){6} as acceptable game code

--- src/main/kotlin/de/wulkanat/extensions/String.kt
+++ src/main/kotlin/de/wulkanat/extensions/String.kt
@@ -1,23 +1,5 @@
 package de.wulkanat.extensions
 
 fun String.isProbablyHash(length: Int): Boolean {
-    if (!(trim() matches "([A-z]|\\d){$length}".toRegex())) return false
-
-    var lastCase: Boolean? = null
-    var consistentCasing = true
-    var numbers = 0
-    forEach {
-        if (it.isDigit()) {
-            numbers++
-        } else {
-            if (lastCase == null) {
-                lastCase = it.isLowerCase()
-            } else {
-                if (lastCase != it.isLowerCase())
-                    consistentCasing = false
-            }
-        }
-    }
-
-    return consistentCasing || numbers > 0
+    return trim() matches "([A-Z]|\\d){$length}".toRegex()
 }
